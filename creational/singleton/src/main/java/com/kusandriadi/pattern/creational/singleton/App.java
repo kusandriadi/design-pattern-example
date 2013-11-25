fe7025ab3644bcbefe8dd3633c36a0dfd5c50e39
Kus Andriadi <kusandriadi@kusandriadi-pc.(none)>
2013-11-26 00:39:47 +0700

    first commit

---
+++ creational/singleton/src/main/java/com/kusandriadi/pattern/creational/singleton/App.java
@@ -0,0 +1,13 @@
+package com.kusandriadi.pattern.creational.singleton;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
